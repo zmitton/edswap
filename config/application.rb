553acd9d0b652b8782fdcb56f6acd946dad3a5c6
cstoneburner <cstoneburner@tradecrafted.com>
2015-12-14 17:24:11 -0800

    color changes and worked on login box

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Edswap
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
