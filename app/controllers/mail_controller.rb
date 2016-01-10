3c5c4f497b82d6601a8e7b2907a69635e506b285
zac <zacmitton22@gmail.com>
2016-01-10 00:29:50 -0600

    WIP mail stuff

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -10,11 +10,11 @@ class MailController < ApplicationController
 
     ListingImage.create
 
-    if message.save
+    # if message.save
       render :text => 'Success', :status => 200
-    else
-      render :text => message.errors.full_messages, :status => 422, :content_type => Mime::TEXT.to_s
-    end
+    # else
+    #   render :text => message.errors.full_messages, :status => 422, :content_type => Mime::TEXT.to_s
+    # end
   end
 
   # def parse
