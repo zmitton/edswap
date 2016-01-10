43c307d42bb48d36934c6352644607b6003a1fb0
zac <zacmitton22@gmail.com>
2016-01-10 00:24:07 -0600

    WIP mail stuff

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,6 +1,24 @@
 class MailController < ApplicationController
   def parse
+    Rails.logger.info params
+    message = Message.new(
+      :to => params[:envelope][:to],
+      :from => params[:envelope][:from],
+      :subject => params[:headers]['Subject'],
+      :body => params[:plain]
+    )
+
     ListingImage.create
-    redirect_to '/'
+
+    if message.save
+      render :text => 'Success', :status => 200
+    else
+      render :text => message.errors.full_messages, :status => 422, :content_type => Mime::TEXT.to_s
+    end
   end
+
+  # def parse
+  #   ListingImage.create
+  #   redirect_to '/'
+  # end
 end
