dd590a57e413408b0d993e079117f3ca20de0b00
zac <zacmitton22@gmail.com>
2016-01-11 11:38:49 -0600

    s3 deafault fixed

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,13 +7,13 @@ class Listing < ActiveRecord::Base
     TempEmailAddress.find_or_create_by(listing_id: id, real_email_address: author.email)
   end
 
-  def main_image
+  def main_image_path
     # if listing_images
     #   listing_images.order(:precedence).first
     # else
     #   ListingImage.find_or_create_by(filename: 'default.png')
     # end
     # ListingImage.first
-    "sample.jpg"
+    "https://s3-us-west-1.amazonaws.com/edswap/listing_images/default.jpg"
   end
 end
