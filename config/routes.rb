5b26b6d1122608d4ecf74b99de683832d4bf2150
zac <zacmitton22@gmail.com>
2016-01-12 23:50:32 -0600

    splash page done

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'welcome#index'
+  root to: 'welcome#splash'
 
 
   resources :sessions, only: [:new, :create, :destroy, :show]
