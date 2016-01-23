8312afe20a70003e02e816ad09a820b9a356db3c
zac <zacmitton22@gmail.com>
2016-01-23 17:51:00 -0600

    email tessts

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -8,24 +8,24 @@ class MailController < ApplicationController
     #   :body => params[:plain]
     # )
 
-    to_temp_address = TempEmailAddress.find_by_temp_email_address(destination_code)
-    if to_temp_address && to_temp_address.listing.active
-      from_temp_address = TempEmailAddress.find_or_create_by(listing_id: to_temp_address.listing_id, real_email_address: from)
-      UserMailer.listing_reply(params, to_temp_address, from_temp_address).deliver_now
+    # to_temp_address = TempEmailAddress.find_by_temp_email_address(destination_code)
+    # if to_temp_address && to_temp_address.listing.active
+      # from_temp_address = TempEmailAddress.find_or_create_by(listing_id: to_temp_address.listing_id, real_email_address: from)
+      UserMailer.listing_reply(params, "to_temp_address", "from_temp_address").deliver_now
       render :text => 'Success', :status => 200
-    else
-      render :text => "temp_email_address_not_found", :status => 422, :content_type => Mime::TEXT.to_s
-    end
+    # else
+    #   render :text => "temp_email_address_not_found", :status => 422, :content_type => Mime::TEXT.to_s
+    # end
   end
 
   private
 
   def destination_code
-    params[:envelope].try(:to).split("@").first
+    params[:envelope][:to].split("@").first
   end
 
   def from
-    params[:envelope].try(:from)
+    params[:envelope][:from]
   end
 
 end
