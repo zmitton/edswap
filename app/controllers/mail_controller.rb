80af2b465c0cffec1bb23c528c0467fac401d725
zac <zacmitton22@gmail.com>
2016-01-23 18:54:10 -0600

    email debugging

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -11,7 +11,7 @@ class MailController < ApplicationController
     to_temp_address = TempEmailAddress.find_by_temp_email_address(destination_code)
     if to_temp_address && to_temp_address.listing.active
       from_temp_address = TempEmailAddress.find_or_create_by(listing_id: to_temp_address.listing_id, real_email_address: from)
-      UserMailer.listing_reply(params, "to_temp_address", "from_temp_address").deliver_now
+      UserMailer.listing_reply(params, to_temp_address, from_temp_address).deliver_now
       render :text => 'Success', :status => 200
     else
       render :text => "temp_email_address_not_found", :status => 422, :content_type => Mime::TEXT.to_s
